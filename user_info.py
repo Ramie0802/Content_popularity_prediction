c79517f41f62d25a7f4f32ed3777313226015eed
Khoi Bui <anhbuikhoi@gmail.com>
2024-09-15 13:06:39 +0900

    update preprocessing file, change embedding file format

--- user_info.py
+++ user_info.py
@@ -1,22 +1,17 @@
 import pandas as pd
 
 
-
-
-class UserInfo:    
+class UserInfo:
 
     def __init__(self):
         pass
 
     @classmethod
-    def load_user_info(cls, name='TMBD'):
+    def load_user_info(cls, name="TMBD"):
 
-        
-        ratings = pd.read_csv(f'./data/{name}.csv')
-        ratings.columns = ['user_id', 'movie_id', 'rating', 'timestamp']
-        
-        user_info = ratings[['user_id']].drop_duplicates()
-        user_info.reset_index(inplace = True, drop = True)
+        ratings = pd.read_csv(f"./data/{name}.csv")
+        ratings.columns = ["user_id", "movie_id", "rating", "timestamp"]
+
+        user_info = ratings[["user_id"]].drop_duplicates()
+        user_info.reset_index(inplace=True, drop=True)
         return user_info
-
-    
